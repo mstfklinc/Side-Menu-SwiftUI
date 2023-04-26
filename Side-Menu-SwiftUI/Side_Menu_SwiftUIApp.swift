47e3bc477ee61fccea4e99571372d728da84aaa2
Mustafa KILINÇ <mustafakilinc429@gmail.com>
2023-04-26 15:48:53 +0300

    Initial Commit

---
+++ Side-Menu-SwiftUI/Side_Menu_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Side_Menu_SwiftUIApp.swift
+//  Side-Menu-SwiftUI
+//
+//  Created by Mustafa Kılınç on 26.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Side_Menu_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
